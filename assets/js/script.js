72a2637c265e2c9fbfd7f3de0015fb5a510cd661
alrivasp <alvaro.rivas.p@gmail.com>
2020-05-01 01:55:33 -0400

    Development extra large view

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+
+    $(function () {
+                
+        /* Carrusel */
+        $('.carousel').carousel({
+            interval: 3000
+        })
+
+        /* ToolTip  */
+        $('[data-toggle="tooltip"]').tooltip()
+
+        
+
+    })
